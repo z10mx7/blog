f3f4c6324f6a9ddb3c6ea9f067bd36060665177c
z10mx7 <z10mx7@protonmail.com>
2023-06-20 13:59:01 +0330

    init design WIP

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,111 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMagnifyingGlass,
+  faPlus,
+  faHeart,
+} from "@fortawesome/free-solid-svg-icons";
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+interface Props {
+  dateString: string;
 }
+
+const formatDate = (dateString: string): string => {
+  const date = new Date(dateString);
+  const options = { day: "2-digit", month: "short", year: "numeric" } as const;
+  return date.toLocaleDateString("en-US", options);
+};
+
+const BlogPost = () => {
+  // const formattedDate = formatDate(dateString);
+  // Sample data for blog posts
+  const blogPosts = [
+    {
+      id: 1,
+      image: "image-url",
+      title: "Blog Post 1",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      tags: ["Tag", "Tag", "Tag"],
+      author: "John Doe",
+      authorImage: "author-image-url",
+      date: "June 1, 2023",
+      likes: 25,
+    },
+    // Add more blog posts here...
+  ];
+
+  return (
+    <div className="max-w-3xl mx-auto mt-16">
+      <div className="flex items-center justify-between p-4">
+        <div className="relative flex items-center">
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            className="absolute left-3 text-gray-400"
+          />
+          <input
+            type="text"
+            placeholder="Search"
+            className="pl-10 pr-4 py-2 border  border-[#E7E7E7]  rounded-3xl focus:outline-none"
+          />
+        </div>
+
+        <button className="px-4 py-2 bg-[#4F73D0] flex items-center justify-between  text-white rounded-full active:bg-[#224DBA]">
+          Add new post
+          <FontAwesomeIcon
+            icon={faPlus}
+            className=" rounded-full p-2   ml-2 text-[#4F73D0] text-sm bg-white "
+          />
+        </button>
+      </div>
+      <div className="grid gap-4 md:grid-cols-2">
+        {blogPosts.map((post) => (
+          <div
+            key={post.id}
+            className="bg-white p-4 rounded-md border-gray-200 border-2"
+          >
+            <img
+              src="http://placeimg.com/640/480/sports"
+              // src={post.image}
+              alt="Blog Post"
+              className="w-full h-40 object-cover rounded-md"
+            />
+            <h2 className="text-xl font-semibold mt-2">{post.title}</h2>
+            <p className="text-[#9D9D9D] text-sm mt-2">{post.description}</p>
+            <div className="flex flex-wrap mt-2">
+              {post.tags.map((tag) => (
+                <span
+                  key={tag}
+                  className="px-4    bg-white text-[#9D9D9D]  border-gray-200 border-2 rounded-3xl  text-sm  "
+                >
+                  {tag}
+                </span>
+              ))}
+            </div>
+            <div className="flex items-center mt-4">
+              <img
+                src="http://placeimg.com/480/480/sports"
+                // src={post.authorImage}
+                alt="Author"
+                className="w-8 h-8 rounded-full mr-2"
+              />
+              <div>
+                <p className="text-sm font-semibold">{post.author}</p>
+                <p className="text-xs text-gray-600">{post.date}</p>
+              </div>
+              <div className="ml-auto flex items-center">
+                <span className="text-sm ml-1 text-gray-400">{post.likes}</span>
+                {/* <img src="/assets/icons/heart-regular.svg" /> */}
+
+                {/* <FontAwesomeIcon icon={faHeart}       className="text-gray-400 bg-white border border-gray-400 rounded-full p-2"  /> */}
+               <svg xmlns="http://www.w3.org/2000/svg" className="ml-1" fill="#9D9D9D" height="1em" viewBox="0 0 512 512"><path d="M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8v-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5v3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20c0 0-.1-.1-.1-.1c0 0 0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5v3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2v-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z" /></svg>
+
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BlogPost;
---
+++ pages/new.tsx
@@ -0,0 +1,69 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSearch,
+  faAmbulance,
+  faArrowLeft,
+  faAnchor,
+  faPlus,faXmark
+} from "@fortawesome/free-solid-svg-icons";
+
+import React from "react";
+
+const FormComponent = () => {
+  return (
+    <div className="max-w-lg mx-auto w-12/12">
+      <div className="flex items-center justify-between p-4 mt-16">
+        <a className="p-2 rounded-full border-2 bg-white w-12 h-12    ">
+          <FontAwesomeIcon icon={faArrowLeft} className="ml-1  mt-1 text-xl" />
+        </a>
+        <h1 className="text-xl font-semibold -ml-44 ">New post</h1>
+        <button className="px-4 py-2 bg-[#4F73D0] text-white rounded-full">
+          Publish post
+        </button>
+      </div>
+      <div className="px-4">
+        <label htmlFor="title" className="block font-normal mb-2 ml-2">
+          Title
+        </label>
+        <input
+          type="text"
+          id="title"
+          className="w-full px-4 py-2 border border-[#E7E7E7] rounded-md focus:outline-none"
+        />
+      </div>
+      <div className="px-4 mt-4">
+        <label htmlFor="description" className="block font-normal mb-2 ml-2">
+          Description
+        </label>
+        <textarea
+          id="description"
+          className="w-full px-4 py-2 border border-[#E7E7E7] rounded-md focus:outline-none resize-none"
+          rows={4}
+        ></textarea>
+      </div>
+      <div className="px-4 mt-4">
+        <label htmlFor="tags" className="block font-normal mb-2 ml-2">
+          Tags
+        </label>
+        <div className="flex items-center border border-[#E7E7E7] rounded-md">
+          <input
+            type="text"
+            id="tags"
+            className="flex-1 px-4 py-2 focus:outline-none"
+          />
+          <button className="px-4 py-2 bg-white text-gray-300 rounded-md">
+            Add
+            <FontAwesomeIcon icon={faPlus} className="ml-2" />
+          </button>
+        </div>
+        <div className="flex items-center mt-2 ">
+          <span className="px-4    bg-white text-[#9D9D9D] pb-1  pt-1 border-gray-200 hover:border-black hover:text-black border-2 rounded-3xl  text-sm  ">
+          <FontAwesomeIcon icon={faXmark} /> Tag
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FormComponent;
